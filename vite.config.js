43081645d89d5db75daecbfeaa6126172a26f683
MURALA SAI GANESH <163727585+MSGanesh23@users.noreply.github.com>
2025-09-25 11:24:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // 👇 Pick up environment variable VITE_BASE from Jenkins, fallback to '/frontapp1/'
-const basePath = process.env.VITE_BASE || '/frontapp1/'
+const basePath = process.env.VITE_BASE || '/'
 
 export default defineConfig({
   base: basePath,
