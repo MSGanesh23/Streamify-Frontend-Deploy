0a81d9147b3c9f8782edef48b38ad7c0221b75f7
MURALA SAI GANESH <163727585+MSGanesh23@users.noreply.github.com>
2025-11-05 09:08:13 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -35,7 +35,7 @@ const Signup = () => {
         }
 
         try {
-            const response = await fetch("http://34.228.12.96:6086/api/register", {
+            const response = await fetch("http://localhost:8080/api/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, username, password, role: 0 }),
